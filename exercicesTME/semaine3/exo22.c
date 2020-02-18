092264214f34ee9acb165c8da153592a95580fb0
Gabriel <gabriel.lahyani@outlook.fr>
2020-02-18 13:58:13 +0200

    #

--- exercicesTME/semaine3/exo22.c
+++ exercicesTME/semaine3/exo22.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define NBTOURS 10
-#define NBLANCERS 2
 #define NBQUILLES 10
-
-/* Il sera probablement n�cessaire de modifier les parametres et valeur de retour des fonctions
-  lancer, lancer_aleatoire, score et jeu */
-
-/* Les declarations actuelles permettent de compiler et d'executer le programme */
-//question1
 int lancer(int a){
   int res;
 printf("veuillez rentrer une valeur supérieure ou égale à 0\n");
@@ -33,17 +25,15 @@ int lancer_aleatoire() {
   return 0;
 }
 //question2
-void score() {
-    int *strike;
-    int *spare;
-    strike=0;
-    spare=0;
+void score(int *strike,int *spare) {
     printf("veuillez rentrer le nombre de quilles que vous avez fait tombé au premier lancer\n");
     int score1=lancer(NBQUILLES);
+    printf("%d",score1);
     if(score1==NBQUILLES){
         *strike=1;
         *spare=0;
         printf("vous avez un strike %d !\n",*strike);
+        return;
 
     }
     printf("Au premier lancer vous avez fait tomber %d quilles \n veuillez rentrer le nombre de quilles que vous avez fait tombé au second lancer\n",score1);
@@ -53,30 +43,29 @@ void score() {
     if(scoretot==NBQUILLES){
         *spare=1;
         *strike=0;
-        printf("vous avez un spare!\n");
+        printf("vous avez un spare %d!\n",*spare);
 
     }
     else{
-        printf("votre score est de %d\n",scoretot);
+        printf("votre score est de %d !\n",scoretot);
 
     }
 
 }
 //question3
-void tour(){
+int tour (){
+  int strike;
+  int spare()
+  int i;
+    score(&strike,&spare);
+  }
 
 
 
 }
+int main(){
+  int strike;
+  int spare;
 
-int jeu() {
-  return 0;
-}
-
-int main() {
-
-
-    score();
-
-  return 0;
+  score(&strike,&spare);
 }
