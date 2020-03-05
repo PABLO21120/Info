67446442bea3a15326b79a79b9a669bbd989254a
Gabriel <gabriel.lahyani@outlook.fr>
2020-03-05 11:56:24 +0200

    exo28tme

--- exercicesTME/semaine5/exo28.c
+++ exercicesTME/semaine5/exo28.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define NB_AMIS 20
 //question1
-void initialise(int ligne,int colonne,int tab[ligne][colonne]){
+void initialise(int nb_amis,int nb_jours,float tab[nb_amis][nb_jours]){
   int i;
   int j;
-  for(i=0;i<ligne;i++){
-    for(j=0;j<colonne;j++){
+  for(i=0;i<nb_amis;i++){
+    for(j=0;j<nb_jours;j++){
 
     tab[i][j]=0;
 }
@@ -15,82 +14,89 @@ void initialise(int ligne,int colonne,int tab[ligne][colonne]){
 }
 
 }
-void affichage(int ligne,int colonne,int tab[ligne][colonne]){
-  int i;
+
+void affichage(int ligne,int colonne,float tab[ligne][colonne]){
+  int i=0;
   int j;
+  printf("\n");
+  while(i<colonne){
+    printf(" \t%3d",i+1);
+    i++;
+  }
+  printf("\n");
+  printf("-----------------------------------------------------------------------");
+  i=0;
+
+  printf("\n");
   for(i=0;i<ligne;i++){
+    printf("%d",i+1);
     for(j=0;j<colonne;j++){
-      printf("%4d",tab[i][j]);
+      printf("\t%.1f",tab[i][j]);
+
     }
+
+    printf("\n");
     printf("\n");
   }
   }
+  //question2
+  void aleatoire(int nb_amis,int nb_jours,float tab[nb_amis][nb_jours]){
+    float montant;
+    int payeur;
+    float res;
+    int i;
+    int x=0;
+    initialise(nb_amis,nb_jours,tab);
+
+    for(i=0;i<nb_jours;i++){
+      montant=rand()%(51-30)+30;
+      payeur=rand()%(nb_amis+1-0)+0;
+      printf("Jour %d : %d paye %.1f\n",i+1,payeur,montant);
+
+      for(x=0;x<nb_amis;x++){
 
 
 
-void ajout(int nb_jours,int nb_amis,int tab[nb_jours][nb_amis]){
-  initialise(nb_jours,nb_amis,tab);
-  int amis;
-  int jours;
-  int prix;
-  int i;
-  printf("veuillez rentrer la personne qui a effectué les dépenses \n");
-  scanf("%d",&amis);
-  printf("veuillez rentrer le jour ou la dépense à ete effectué  \n");
-  scanf("%d",&jours);
-  printf("veuillez rentrer le montant de la dépense \n");
-  scanf("%d",&prix);
-  tab[jours][amis]=prix-(prix/(nb_amis-1));
-  for(i=0;i<nb_amis;i++){
+        if((payeur-1)==x){
 
-      printf("helloworld\n");
-      printf("%d, %d\n",amis,i);
-      if(i!=amis){
-        tab[jours][i]=-(prix/(nb_amis-1));
+          tab[x][i]=montant-(montant/nb_amis);
+        }else{
+          tab[x][i]=-(montant/nb_amis);
+        }
 
-
-      }
-
-
-  }
-  affichage(nb_jours,nb_amis,tab);
-}
-//question2
-void aleatoire(int j,int nb_jours,int nb_amis,int tab[nb_jours][nb_amis]){
-  initialise(nb_jours,nb_amis,tab);
-    int montant;
-    int amis;
-
-  int i;
-
-  tab[j][amis]=montant-(montant/(nb_amis-1));
-  for(i=0;i<nb_jours;i++){
-    montant=rand()%(51-30)+30;
-     amis=rand()%(nb_amis+1-0)+0;
-      printf("Jour %d : %d paye %d \n",i+1,amis,montant);
-      tab[j][amis]=montant-(montant/(nb_amis-1));
-      if(i!=amis){
-        tab[j][i]=-(montant)/(nb_amis-1);
 
       }
 
     }
+    affichage(nb_amis,nb_jours,tab);
 
-
-  printf("%d,%d\n",montant,amis);
-  affichage(nb_jours,nb_amis,tab);
-
-
-}
-int main(){
-  int ligne=6;
-  int colonne=8;
+  }
+//question4
+float bills(int nb_amis,int nb_jours,float tab[nb_amis][nb_jours]){
+  aleatoire(nb_amis,nb_jours,tab);
+  int i;
   int j;
-  printf("veuillez rentrer le jour ou la dépense à ete effectué  \n");
-  scanf("%d",&j);
-  int tab[ligne][colonne];
-  srand(time(NULL));
-  aleatoire(j,ligne,colonne,tab);
+  float res;
+  for(j=0;j<nb_amis;j++){
+  for(i=0;i<nb_jours;i++){
+      res+=tab[j][i];
+    }
+    printf("l'ami n°%d à un solde de %.1f\n",j+1,-res);
 
+  }
+  printf("si le montant est négatif cela veut dire que on lui doit de l'argent sinon il en doit !\n");
+
+  return 0;
+}
+
+int main(){
+  int j=4;
+  int nb_amis=5;
+  int nb_jours=7;
+  float tab[nb_amis][nb_jours];
+  bills(nb_amis,nb_jours,tab);
+  srand(time(NULL));
+
+  return 0;
 
 }
